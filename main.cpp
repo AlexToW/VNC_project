43eefdce8a721e9a2fae66a600adcbe234ce1c1d
AlexToW <trishin.as@phystech.edu>
2022-02-21 02:24:09 +0300

    takes screenshot

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QScreen>
+#include <QWindow>
+#include <QDir>
+#include <iostream>
+#include "main.moc"
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QScreen *screen = a.primaryScreen();
+    QPixmap pixmap = screen->grabWindow(0);
+    const QString format = "png";
+    QString path = QDir::currentPath() + "\\screen." + format;
+    std::cout << path.toStdString().c_str() << std::endl;
+
+    if(pixmap.save(path)) {
+        std::cout << "Saved!" << std::endl;
+    } else {
+        std::cout << "Didn't saved!" << std::endl;
+    }
+
+    return a.exec();
+}
